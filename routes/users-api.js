307363179b4b3c008a28860f60336a8bb6565545
Victor Anogwih <anogwihvictor@gmail.com>
2024-03-06 18:48:19 -0500

    create user api added

--- routes/users-api.js
+++ routes/users-api.js
@@ -7,7 +7,10 @@
 
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require("bcrypt");
 const userQueries = require('../db/queries/users');
+const userReg = require('../db/queries/addUsers');
+
 
 router.get('/', (req, res) => {
   userQueries.getUsers()
@@ -19,6 +22,22 @@ router.get('/', (req, res) => {
         .status(500)
         .json({ error: err.message });
     });
+});
+
+//create a user route
+router.post("/addusers", (req, res) => {
+  const user = req.body;
+  if (!user.username || !user.email || !user.user_password) {
+    return res.send({error: "Enter registration details"});
+  }
+  user.user_password = bcrypt.hashSync(user.user_password, 12);
+  userReg
+    .addUser(user)
+    .then((user) => {
+      req.session.userId = user.id;
+      res.send("User Created");
+    })
+    .catch((e) => res.send(e));
 });
 
 module.exports = router;
