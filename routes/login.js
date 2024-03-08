656cc1f8fe64fcaba1da5de88981e37fc50c1e46
Jan Edmark Arieta <edarieta09@gmail.com>
2024-03-08 07:21:31 +0000

    fixes log in bug not redirecting

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { findUser } = require('../db/queries/findUser')
 
 router.get('/', (req, res) => {
