60574c90a5b397333589f7eae69ee88c1cc9b9cc
Victor Anogwih <anogwihvictor@gmail.com>
2024-03-07 23:05:23 -0500

    modified the register route to hash passwords and to use cookies session to store user id(s)

--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const cookieSession  = require('cookie-session')
 const { addUser } = require('../db/queries/addUsers');
+
+router.use(cookieSession ({
+  name: 'session',
+  keys: ['key1', 'key2'],
+  maxAge: 24 * 60 * 60 * 1000, // 24 hours
+}));
 
 router.get('/', (req, res) => {
   res.render('register');
@@ -12,9 +20,14 @@ router.post('/', (req, res) => {
     email: req.body.email,
     user_password: req.body.password
    };
+   if(!user.username || !user.email || !user.user_password) {
+    return res.send({error: "Enter registratin details"});
+   }
+   user.user_password = bcrypt.hashSync(user.user_password, 12);
 
    addUser(user)
      .then(user => {
+      req.session.userId = user.id;
       res.redirect('/login');
      })
      .catch(err => {
